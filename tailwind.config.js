1277cf0ae078ffe5802b1e3c855721eb88be5d53
MeongGanas <farreluken@gmail.com>
2024-09-01 20:28:04 +0800

    menghapus custom col span karena tidak bekerja

--- tailwind.config.js
+++ tailwind.config.js
@@ -77,9 +77,6 @@ export default {
                 "accordion-down": "accordion-down 0.2s ease-out",
                 "accordion-up": "accordion-up 0.2s ease-out",
             },
-            gridColumn: {
-                "span-1.5": "span 1.5 / span 1.5",
-            },
         },
     },
 
